428893576d6d93d758156f812e5d9eb16ccc5c2c
KKBUGHUNTER <karthikeyan.aas@gmail.com>
2024-12-17 19:43:05 +0530

    firmware update

---
+++ firmware/include/configs/FirebaseConfig.h
@@ -0,0 +1,9 @@
+
+
+#define API_KEY "AIzaSyDgwZKG93XVmjSwBvUfbIB96FE5gtbO_V8"
+#define DATABASE_URL "https://iet-office-tsy-e1e18-default-rtdb.asia-southeast1.firebasedatabase.app/"
+/* Firebase credentials project-1*/
+// #define API_KEY "AIzaSyBm_I3tnxfiSi8sd-jhiFNZpO5XgEj8NJ4"
+// #define DATABASE_URL "https://project1-ac5cd-default-rtdb.asia-southeast1.firebasedatabase.app/"
+#define USER_EMAIL "device@gmail.com"
+#define USER_PASSWORD "device"
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,111 +1,116 @@
+
 #include <Arduino.h>
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
+
 #include <FirebaseClient.h>
 
-// Unique Device ID
 #define DEVICE_ID "IET-a-7021"
-
-/* WiFi credentials */
 #define WIFI_SSID "loop"
 #define WIFI_PASSWORD "cyber@123"
 
-/* Firebase credentials */
-#define API_KEY "AIzaSyDgwZKG93XVmjSwBvUfbIB96FE5gtbO_V8"
-#define DATABASE_URL "https://iet-office-tsy-e1e18-default-rtdb.asia-southeast1.firebasedatabase.app/"
-
-/* Firebase credentials project-1*/
-// #define API_KEY "AIzaSyBm_I3tnxfiSi8sd-jhiFNZpO5XgEj8NJ4"
-// #define DATABASE_URL "https://project1-ac5cd-default-rtdb.asia-southeast1.firebasedatabase.app/"
-
-#define USER_EMAIL "device@gmail.com"
-#define USER_PASSWORD "device"
+#include <configs/FirebaseConfig.h>
 
 void asyncCB(AsyncResult &aResult);
-void printResult(AsyncResult &aResult);
+void asyncCBJust(AsyncResult &aResult);
 
-DefaultNetwork network; // Network handler
-UserAuth user_auth(API_KEY, USER_EMAIL, USER_PASSWORD);
-FirebaseApp app;
+// Firebase Auth
+UserAuth user_auth(API_KEY, USER_EMAIL, USER_PASSWORD, 3000);
+
+//
+DefaultNetwork network;
 WiFiClientSecure ssl_client1, ssl_client2;
+AsyncClientClass aClient1(ssl_client1, getNetwork(network)), aClient2(ssl_client2, getNetwork(network));
 
-using AsyncClient = AsyncClientClass;
-
-AsyncClient aClient(ssl_client1, getNetwork(network)), aClient2(ssl_client2, getNetwork(network));
+FirebaseApp app;
 RealtimeDatabase Database;
 
-unsigned long ms = 0;
+unsigned long tmo = 0;
 
 void setup()
 {
-  Serial.begin(115200);
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
-  Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(300);
-  }
-  Serial.println();
+    Serial.begin(115200);
 
-  Serial.println("✔ Device Connected to Internet Successfully...");
-  Serial.print("Connected with IP: ");
-  Serial.println(WiFi.localIP());
-  Serial.println();
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
-  Firebase.printf("Firebase Client v%s\n", FIREBASE_CLIENT_VERSION);
-  Serial.println("Initializing app...");
+    Serial.print("Connecting to Wi-Fi");
+    unsigned long ms = millis();
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(300);
+    }
+    Serial.println();
+    Serial.print("Connected with IP: ");
+    Serial.println(WiFi.localIP());
+    Serial.println();
 
-  ssl_client1.setInsecure();
-  ssl_client2.setInsecure();
+    Firebase.printf("Firebase Client v%s\n", FIREBASE_CLIENT_VERSION);
+    Serial.println("Initializing app...");
+    ssl_client1.setInsecure();
+    ssl_client2.setInsecure();
 
-  initializeApp(aClient2, app, getAuth(user_auth), asyncCB, "authTask");
+    // Initialize the FirebaseApp or auth task handler.
+    // To deinitialize, use deinitializeApp(app).
+    initializeApp(aClient2, app, getAuth(user_auth), asyncCB, "authTask");
+    initializeApp(aClient1, app, getAuth(user_auth), asyncCB, "authTask");
 
-  app.getApp<RealtimeDatabase>(Database);
-  Database.url(DATABASE_URL);
+    // Binding the FirebaseApp for authentication handler.
+    // To unbind, use Database.resetApp();
+    app.getApp<RealtimeDatabase>(Database);
+    Database.url(DATABASE_URL);
 
-  Database.setSSEFilters("get,put,patch,keep-alive,cancel,auth_revoked");
-  Database.get(aClient, "/devices/IET-a-7021", asyncCB, true /* SSE mode */, "streamTask");
-  Serial.println("✔ Device Connected to Firebase Successfully...");
+    Database.setSSEFilters("get,put,patch,keep-alive,cancel,auth_revoked");
+    Database.get(aClient1, "/devices/IET-a-7021", asyncCB, true, "ListenerTask");
 }
 
 void loop()
 {
-  app.loop();
-  Database.loop();
+    app.loop();
+    Database.loop();
+
+    if (millis() - tmo > 20000 && app.ready())
+    {
+        tmo = millis();
+
+        JsonWriter writer;
+
+        object_t json, obj1, obj2;
+
+        writer.create(obj1, "ms", tmo);
+        writer.create(obj2, "rand", random(10000, 30000));
+        writer.join(json, 2, obj1, obj2);
+
+        Database.set<object_t>(aClient2, "/test/stream/number", json, asyncCBJust);
+    }
 }
 
 void asyncCB(AsyncResult &aResult)
 {
-  if (aResult.available())
-  {
-    printResult(aResult);
-  }
-  else
-  {
-    Serial.println("No data found");
-  }
+    // WARNING!
+    // Do not put your codes inside the callback.
+
+    if (aResult.isEvent())
+        Firebase.printf("-> Event task: %s, msg: %s, code: %d\n", aResult.uid().c_str(), aResult.appEvent().message().c_str(), aResult.appEvent().code());
+
+    if (aResult.isDebug())
+        Firebase.printf("-> Debug task: %s, msg: %s\n", aResult.uid().c_str(), aResult.debug().c_str());
+
+    if (aResult.isError())
+        Firebase.printf("-> Error task: %s, msg: %s, code: %d\n", aResult.uid().c_str(), aResult.error().message().c_str(), aResult.error().code());
+
+    if (aResult.available())
+        Firebase.printf("-> task: %s, payload: %s\n", aResult.uid().c_str(), aResult.c_str());
 }
 
-void printResult(AsyncResult &aResult)
+void asyncCBJust(AsyncResult &aResult)
 {
-  if (aResult.available())
-  {
-    RealtimeDatabaseResult &RTDB = aResult.to<RealtimeDatabaseResult>();
-    if (RTDB.isStream())
-    {
-      Firebase.printf("task: %s - ", aResult.uid().c_str());
-      Firebase.printf("event: %s - ", RTDB.event().c_str());
-      Firebase.printf("path: %s - ", RTDB.dataPath().c_str());
-      Firebase.printf("data: %s\n", RTDB.to<const char *>());
-      Firebase.printf("type: %d\n", RTDB.type());
-      Serial.println("----------------------------");
-    }
-    else
-    {
-      Serial.println("----------------------------");
-      Firebase.printf("task: %s, payload: %s\n", aResult.uid().c_str(), aResult.c_str());
-    }
-  }
+    // WARNING!
+    // Do not put your codes inside the callback.
+
+    if (aResult.isEvent())
+        Firebase.printf("--> Event task: %s, msg: %s, code: %d\n", aResult.uid().c_str(), aResult.appEvent().message().c_str(), aResult.appEvent().code());
+    if (aResult.available())
+        Firebase.printf("--> task: %s, payload: %s\n", aResult.uid().c_str(), aResult.c_str());
 }
